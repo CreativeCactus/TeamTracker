41aeb3348e60dd0870257f13aaba968defe96951
root <root@metaboard.com.au>
2017-10-02 04:32:24 +0000

    Script work...

--- scripts/createHook.sh
+++ scripts/createHook.sh
@@ -1,8 +1,9 @@
 if [[ $# -lt 2 ]] ; then
 	echo './createHook.sh "User/Repo" "hookUrl"'
-	echo './createHook.sh "CreativeCactus/BoBot" "http://`hostname -I`:7777/webhook"'
+	echo './createHook.sh "CreativeCactus/BoBot" "http://$(hostname -I):7777/webhook"'
 	exit 1
 fi
 
-curl -X POST "https://api.github.com/repos/${0}/hooks" -H "Content-Type: application/json" -d \
-	'{"name":"web", "active": true, "events": ["push","pull_request"], "config": {"url": "'${1}'","content_type": "json"}}'
+set -x
+curl -X POST "https://api.github.com/repos/${1}/hooks" -H "Content-Type: application/json" -d \
+	'{"name":"web", "active": true, "events": ["push","pull_request"], "config": {"url": "'${2}'","content_type": "json"}}'
